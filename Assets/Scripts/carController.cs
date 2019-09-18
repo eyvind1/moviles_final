cb9100fb55662a1f2962de4814f207d8e3367f9b
Julio Jesus <julio31529@gmail.com>
2019-09-18 15:03:14 -0500

    added rigidbody

--- Assets/Scripts/carController.cs
+++ Assets/Scripts/carController.cs
@@ -4,14 +4,27 @@ using UnityEngine;
 
 public class carController : MonoBehaviour {
 
-	
-	
-	// Update is called once per frame
-	void Update () {
-		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
-        transform.Rotate(0, x, 0);
-        transform.Translate(0, 0, z);
+    public bool isFlat = true;
+    private Rigidbody rigid;
+	// Update is called once per frame
+
+    private void start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+	private void Update () {
+		//var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        //var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        //transform.Rotate(0, x, 0);
+        //transform.Translate(0, 0, z);
+
+        Vector3 tilt = Input.acceleration;
+        if (isFlat)
+            tilt = Quaternion.Euler(90, 0, 0) * tilt;
+        rigid.AddForce(tilt);
+        Debug.DrawRay(transform.position + Vector3.up, tilt, Color.cyan);
 	}
 }
