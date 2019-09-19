118b509621028bc32e429e92eb956282749f66ee
eyvind1 <eyvind.osco@ucsp.edu.pe>
2019-09-19 11:52:54 -0500

    fix_errors

--- Assets/Scripts/carController.cs
+++ Assets/Scripts/carController.cs
@@ -5,26 +5,25 @@ using UnityEngine;
 public class carController : MonoBehaviour {
 
 
-    public bool isFlat = true;
-    private Rigidbody rigid;
+    //private Rigidbody rigid;
+    //float dirx;
+    //float moveSpeed = 20f;
 	// Update is called once per frame
 
     private void start()
     {
-        rigid = GetComponent<Rigidbody>();
+        //rigid = GetComponent<Rigidbody>();
     }
 
 	private void Update () {
 		//var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
         //var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
-
+        transform.Translate(Input.acceleration.x,0,-Input.acceleration.z);
         //transform.Rotate(0, x, 0);
         //transform.Translate(0, 0, z);
+        
 
-        Vector3 tilt = Input.acceleration;
-        if (isFlat)
-            tilt = Quaternion.Euler(90, 0, 0) * tilt;
-        rigid.AddForce(tilt);
-        Debug.DrawRay(transform.position + Vector3.up, tilt, Color.cyan);
+
 	}
+    
 }
