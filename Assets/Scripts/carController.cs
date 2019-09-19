26e7a0c7ff63216775ac08fcbb5422c4b6a1863e
eyvind1 <eyvind.osco@ucsp.edu.pe>
2019-09-19 12:15:37 -0500

    accelerometer

--- Assets/Scripts/carController.cs
+++ Assets/Scripts/carController.cs
@@ -7,7 +7,7 @@ public class carController : MonoBehaviour {
 
     //private Rigidbody rigid;
     //float dirx;
-    //float moveSpeed = 20f;
+    float moveSpeed = 0.5f;
 	// Update is called once per frame
 
     private void start()
@@ -18,7 +18,9 @@ public class carController : MonoBehaviour {
 	private void Update () {
 		//var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
         //var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
-        transform.Translate(Input.acceleration.x,0,-Input.acceleration.z);
+        
+        transform.Translate(Input.acceleration.x*moveSpeed,0,0);
+        
         //transform.Rotate(0, x, 0);
         //transform.Translate(0, 0, z);
         
